5d9844fdfa7edb87691682831163a01de7da291c
Azrapse <azrapse@gmail.com>
2014-02-28 15:54:59 +0200

    Adapt to Require.js, jQuery 1.11 (migrate), Modernzr

---
+++ tor/js/app.js
@@ -0,0 +1,21 @@
+requirejs.config({
+    "baseUrl": "js/lib",
+    "paths": {
+        "app": "../app",
+        "jquery": "jquery-1.11.0",
+        "jquery.ui": "jquery-ui-1.10.4.custom.min",
+        "jquery.linq": "jquery.linq.min",
+        "jquery.migrate": "jquery-migrate-1.2.1",
+        "modernizr": "modernizr-1.7.min",
+        "json": "json-serialization"
+    },
+    "shim": {
+        "jquery.linq": ["jquery"],
+        "jquery.ui": ["jquery"],
+        "jquery.cookies": ["jquery"],
+        "jquery.migrate": ["jquery"]
+    }
+});
+
+// Load the main app module to start the app
+requirejs(["app/code"]);
